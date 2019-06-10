bb59ee524e7b4a5a01e131c99884c6cb2c9fdb2a
espstan <espstan@gmail.com>
2019-06-10 14:02:52 +0300

    ISSUE-5 Добавлена конфигурация БД

--- blog/config.py
+++ blog/config.py
@@ -7,13 +7,5 @@ class Configuration(object):
     #db settings
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     basedir = os.path.abspath(os.path.dirname(__file__))
-    print(basedir)
-
-    SQLALCHEMY_DATABASE_URI = os.path.join('sqlite:///', basedir, 'blog.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'blog.db')
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
-
-
-    print(SQLALCHEMY_DATABASE_URI)
-    print(SQLALCHEMY_MIGRATE_REPO)
-
-
