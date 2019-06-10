f3817b380e5352716c80910eae415e79f402d218
espstan <espstan@gmail.com>
2019-06-10 18:26:22 +0300

    ISSUE-17 Подключены библиотеки migrate, script, добавлен импорт Post

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
+from flask_migrate import Migrate
+from flask_migrate import MigrateCommand
+
+from flask_script import Manager
+
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Configuration
@@ -9,3 +14,11 @@ app = Flask(__name__)
 
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
+
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+from models import Post
