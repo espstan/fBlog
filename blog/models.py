c937a367b3abb800842d4f86570d35faf3929132
espstan <espstan@gmail.com>
2019-06-10 15:37:18 +0300

    ISSUE-6 Создан файл models.py, создан класса Post

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy.sql.functions import current_timestamp
+
+from app import db
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=current_timestamp())
+    is_published = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, *args, **kwargs):
+        super(Post, self).__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return "<Post id: {}, title: {}>".format(self.id, self.title)
