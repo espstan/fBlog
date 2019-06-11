3f8de738efc28ffca6fc9b7e319775d11d250f5e
espstan <espstan@gmail.com>
2019-06-11 17:42:49 +0300

    ISSUE-22 Добавлен класс Comment

--- blog/models.py
+++ blog/models.py
@@ -28,3 +28,16 @@ class Tag(db.Model):
 
     def __repr__(self):
         return '<Tag id: {}, name: {}>'.format(self.id, self.name)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    email = db.Column(db.String(254))
+    body = db.Column(db.Text)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return '<Comment id: {}, name: {}>'.format(self.id, self.name)
