b0b0b6d5c0474b3172a5885ed36903c163cd6200
espstan <espstan@gmail.com>
2019-06-11 16:37:27 +0300

    ISSUE-_19 Фиксация текущих изменений

--- blog/posts/posts.py
+++ blog/posts/posts.py
@@ -11,11 +11,17 @@ from models import Post
 from config import Configuration
 
 
-#POST - создать новый пост=создать черновик
+@app.route('/')
+def home():
+    return "<h1>Hello, fBlog</h1>"
+
+
+# POST - создать новый пост=создать черновик
 @app.route('/posts', methods=['POST'])
 def create_post():
     request_data = request.get_json()
-
+    print("Hello")
+    print(request_data)
     if request_data['title']:
         if len(request_data['title']) <= Configuration.MAX_TITLE_SIZE:
             try:
@@ -31,3 +37,6 @@ def create_post():
         else:
             return jsonify({'message': 'len(title) > {}'.format(Configuration.MAX_TITLE_SIZE)})
     return jsonify({'message': 'your post has no title'})
+
+
+
