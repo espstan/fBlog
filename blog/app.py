68eb1fd13093dde66d6c309db2a4e661e63c2fd1
espstan <espstan@gmail.com>
2019-06-13 16:41:10 +0300

    ISSUE-29 Добавлен импорт и ресурс PostStatistics

--- blog/app.py
+++ blog/app.py
@@ -9,8 +9,9 @@ from flask_script import Manager
 
 from config import Configuration
 
-from resources.post import PostRegister
 from resources.post import PostList
+from resources.post import PostRegister
+from resources.post import PostStatistics
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -24,6 +25,7 @@ def create_tables():
 
 api.add_resource(PostRegister, '/post')
 api.add_resource(PostList, '/posts')
+api.add_resource(PostStatistics, '/statistics')
 
 if __name__ == '__main__':
     from db import db
