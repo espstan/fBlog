73c1704a9c583d13b18625aba2926e9387a8b8ac
espstan <espstan@gmail.com>
2019-06-14 19:52:58 +0300

    ISSUE-34 Отредактирован db.py (миграции закомментированы)

--- blog/db.py
+++ blog/db.py
@@ -1,3 +1,15 @@
+from flask_migrate import Migrate
+from flask_migrate import MigrateCommand
+
+from flask_script import Manager
+
 from flask_sqlalchemy import SQLAlchemy
 
+from app import app
+
 db = SQLAlchemy()
+
+
+manager = Manager(app)
+# migrate = Migrate(app, db)
+# manager.add_command('db', MigrateCommand)
