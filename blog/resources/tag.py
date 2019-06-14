6649d8f80d3c0d138cb7d18e993fca728ed40838
espstan <espstan@gmail.com>
2019-06-14 19:56:31 +0300

    ISSUE-34 Созданы ресурсы класса TagModel

--- blog/resources/tag.py
+++ blog/resources/tag.py
@@ -5,30 +5,35 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from config import Configuration
 
-from models.post import PostModel
-
 from models.tag import TagModel
 
 
 class TagRegister(Resource):
+    def post(self, name):
+        if len(name) > Configuration.MAX_TAG_NAME_SIZE:
+            return {'message': 'A name\'s length is more than {}'.format(Configuration.MAX_TAG_NAME_SIZE)}
 
+        if name in TagModel.get_tags():
+            return {'message': 'Tag name already exists'}
 
+        new_tag = TagModel(name)
+        try:
+            new_tag.save_to_db()
+        except SQLAlchemyError as e:
+            err = str(e.__class__.__name__)
+            return {'message': '{}'.format(err)}, 500
+        return new_tag.get_json(), 201
 
+    def get(self, name):
+        pass
+
+    def put(self, name):
+        pass
 
     def delete(self, name):
-
         pass
 
 
-    def put(self, _id):
-        data = Post.parser.parse_args()
-        post = PostModel.find_by_id(_id)
-        if not post:
-            post = PostRegister.post()
-        else:
-            post.title = data['title']
-            post.body = data['body']
-            post.user_id = data['user_id']
-            post.is_published = data['is_published']
-        post.save_to_db()
-        return item.json(), 200
+class TagList(Resource):
+    def get(self):
+        return {'tags': [tag.get_json() for tag in TagModel.query.all()]}
