5074bb9408e815edeb428728bce9bc71d1c4d69e
espstan <espstan@gmail.com>
2019-06-16 14:48:08 +0300

    ISSUE-52 Добавлены импорты и ресурсы CategoryList

--- blog/app.py
+++ blog/app.py
@@ -5,6 +5,7 @@ from flask_restful import Api
 from config import Configuration
 
 from resources.category import Category
+from resources.category import CategoryList
 from resources.post import Post
 from resources.post import PostList
 from resources.post import PostRegister
@@ -23,6 +24,7 @@ def create_tables():
 
 
 api.add_resource(Category, '/category/<string:name>')
+api.add_resource(CategoryList, '/categories')
 api.add_resource(Post, '/post/<int:_id>')
 api.add_resource(PostRegister, '/post')
 api.add_resource(PostList, '/posts')
