aa6b578867527b6413d880214cb88cef71712b99
espstan <espstan@gmail.com>
2019-06-17 09:59:21 +0300

    ISSUE-72 Изменены методы класса русурса Comment

--- blog/resources/comment.py
+++ blog/resources/comment.py
@@ -19,8 +19,7 @@ class Comment(Resource):
 
     parser.add_argument('email',
                         type=str,
-                        required=True,
-                        help='This field cannot be blank.')
+                        required=False)
 
     parser.add_argument('body',
                         type=str,
@@ -32,7 +31,7 @@ class Comment(Resource):
                         required=True,
                         help='This field cannot be blank.')
 
-    def post(self, _id):
+    def post(self):
         data = Comment.parser.parse_args()
 
         if len(data['name']) > Configuration.MAX_COMMENT_NAME_SIZE:
@@ -44,7 +43,11 @@ class Comment(Resource):
         if not PM.find_by_id(data['post_id']):
             return {'message': 'There is no such post: \'{}\''.format(data['post_id'])}
 
+        post_id = data['post_id']
+        del data['post_id']
         comment = CM(**data)
+
+        comment.post_id = post_id
         try:
             comment.save_to_db()
         except SQLAlchemyError as e:
@@ -52,11 +55,63 @@ class Comment(Resource):
             return {'message': '{}'.format(err)}, 500
         return comment.get_json(), 201
 
-    def put(self, name):
-        pass
 
-    def delete(self, name):
-        pass
+class CommentRegistry(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name',
+                        type=str,
+                        required=True,
+                        help='This field cannot be blank.')
+
+    parser.add_argument('email',
+                        type=str,
+                        required=False)
+
+    parser.add_argument('body',
+                        type=str,
+                        required=True,
+                        help='This field cannot be blank.')
+
+    parser.add_argument('post_id',
+                        type=int,
+                        required=True,
+                        help='This field cannot be blank.')
+
+    def put(self, _id):
+        data = CommentRegistry.parser.parse_args()
+
+        if len(data['name']) > Configuration.MAX_COMMENT_NAME_SIZE:
+            return {'message': 'A name\'s length is more than {}'.format(Configuration.MAX_COMMENT_NAME_SIZE)}
+
+        post_id = data['post_id']
+        del data['post_id']
+        comment = CM.find_by_id(_id)
+
+        if not comment:
+            comment = CM(**data)
+        else:
+            comment.name = data['name']
+            comment.email = data['email']
+            comment.body = data['body']
+            comment.post_id = post_id
+
+        try:
+            comment.save_to_db()
+        except SQLAlchemyError as e:
+            err = str(e.__class__.__name__)
+            return {'message': '{}'.format(err)}, 500
+        return comment.get_json(), 201
+
+    def delete(self, _id):
+        comment = CM.find_by_id(_id)
+        if comment:
+            try:
+                comment.delete_from_db()
+            except SQLAlchemyError as e:
+                err = str(e.__class__.__name__)
+                return {'message': '{}'.format(err)}, 500
+            return {'message': 'Comment was deleted'}
+        return {'message': 'Comment with name: \'{}\' was not found'.format(name)}
 
 
 class CommentList(Resource):
