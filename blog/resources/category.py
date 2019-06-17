f94226651abec40cd9bd980987c25c6fd45cb5fe
espstan <espstan@gmail.com>
2019-06-17 03:06:27 +0300

    ISSUE-48 Изменены методы post, put, delete

--- blog/resources/category.py
+++ blog/resources/category.py
@@ -7,11 +7,14 @@ from config import Configuration
 
 from models.category import CategoryModel as CM
 
+from models.post import PostModel as PM
+
 
 class Category(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('name',
-                        type=str,
+    parser.add_argument('posts',
+                        type=int,
+                        action='append',
                         required=True,
                         help='This field cannot be blank.')
 
@@ -23,8 +26,23 @@ class Category(Resource):
         if len(name) > Configuration.MAX_CATEGORY_NAME_SIZE:
             return {'message': 'A title\'s length is more than {}'.format(Configuration.MAX_CATEGORY_NAME_SIZE)}
 
+        data = Category.parser.parse_args()
+        posts_id = data['posts']
+        posts = []
+        for _id in posts_id:
+            post = PM.find_by_id(_id)
+            if post:
+                posts.append(post)
+            else:
+                return {'message': 'There is no such post: \'{}\''.format(_id)}
+
         new_category = CM(name=name)
 
+        for post in set(posts):
+            if PM.query.filter(PM.id == post.id).first():
+                new_category.posts.append(post)
+            else:
+                return {'message': 'There is no such post: \'{}\''.format(post.id)}
         try:
             new_category.save_to_db()
         except SQLAlchemyError as e:
@@ -41,35 +59,42 @@ class Category(Resource):
                 err = str(e.__class__.__name__)
                 return {'message': '{}'.format(err)}, 500
             return {'message': 'Category was deleted'}
-        return {'message': 'Category with name={} was not found'.format(name)}
+        return {'message': 'Category with name: \'{}\' was not found'.format(name)}
 
     def put(self, name):
         data = Category.parser.parse_args()
-        new_name = data['name']
 
         if len(name) > Configuration.MAX_CATEGORY_NAME_SIZE:
             return {'message': 'A name\'s length is more than {}'.format(Configuration.MAX_CATEGORY_NAME_SIZE)}
 
-        if len(new_name) > Configuration.MAX_CATEGORY_NAME_SIZE:
-            return {'message': 'A name\'s length is more than {}'.format(Configuration.MAX_CATEGORY_NAME_SIZE)}
-
-        if CM.find_by_name(new_name):
-            return {'message': 'A category with name \'{}\' already exists'.format(new_name)}, 400
-
-        if name:
-            category = CM.find_by_name(name)
-            if category:
-                category.name = new_name
-                try:
-                    category.save_to_db()
-                except SQLAlchemyError as e:
-                    err = str(e.__class__.__name__)
-                    return {'message': '{}'.format(err)}, 500
-                return category.get_json(), 201
+        posts_id = data['posts']
+        posts = []
+        for _id in posts_id:
+            post = PM.find_by_id(_id)
+            if post:
+                posts.append(post)
             else:
-                return {'message': 'No such category: \'{}\''.format(name)}, 400
+                return {'message': 'There is no such post: \'{}\''.format(_id)}
+
+        category = CM.find_by_name(name)
+        if not category:
+            category = CM(name=name)
         else:
-            return {'message': 'Incorrect category\'s name: {}'.format(name)}
+            category.name = name
+
+        category.posts = []
+        for post in set(posts):
+            if PM.query.filter(PM.id == post.id).first():
+                category.posts.append(post)
+            else:
+                return {'message': 'There is no such post: \'{}\''.format(post.id)}
+
+        try:
+            category.save_to_db()
+        except SQLAlchemyError as e:
+            err = str(e.__class__.__name__)
+            return {'message': '{}'.format(err)}, 500
+        return category.get_json(), 201
 
 
 class CategoryList(Resource):
